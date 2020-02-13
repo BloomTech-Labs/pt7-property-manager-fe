0b8c6b168fa3cbbb25e7d340d6ea1bf0182cbf5d
Caleb Redd <caleb.redd@yahoo.com>
2020-02-12 19:51:28 -0700

    General react router nav setup and components schedule

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./App.scss";
+import Navigation from "./components/navigation/";
+import Home from "./components/home/";
+import Login from "./components/login/";
 
 function App() {
+  const [user, setUser] = useState({
+    username: localStorage.getItem("username"),
+    user_id: localStorage.getItem("user+id")
+  });
+
   return (
     <div className="App">
       <header className="App-header">My Property Manager</header>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Login" component={Login} />
+      </Switch>
     </div>
   );
 }
