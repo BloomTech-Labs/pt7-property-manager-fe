7616f4168ee13dc5590a7011ba351fbf921adbbe
Carlos Sanchez <carlo.s16@hotmail.com>
2020-02-15 19:56:31 -0500

    added main section to landing page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import './homepage.scss'
 
 export default function Home() {
   return (
-    <div>
-      <h2>Home Page</h2>
-    </div>
+    <section className="mainContent">
+      <div className="heading w-50">
+        <h1>Find your new home with <br></br>
+        Property Manager</h1>
+        <button className="getStarted">Get Started</button>
+      </div>
+      <div className="icon w-50"></div>
+    </section>
   );
 }
