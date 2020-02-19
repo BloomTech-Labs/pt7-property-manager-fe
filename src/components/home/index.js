1f52d2abf7d39e6fd3cb9291e337bd1bf944dad6
Carlos Sanchez <carlo.s16@hotmail.com>
2020-02-18 22:27:49 -0500

    added new styling to make the page responsive. need to work on footer

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -4,22 +4,21 @@ import './homepage.scss'
 export default function Home() {
   return (
     <div>
-      <section className="mainContent">
-        <div className="heading w-50">
-          <h1>Find your new home with <br></br>
+      <section className="mainContent contentSection">
+        <div className="heading">
+          <h1 className="header">Find your new home with <br></br>
           Property Manager</h1>
           <button className="getStarted">Get Started</button>
         </div>
-        <div classNam e="icon w-50"></div>
       </section>
-      <section className="propertyManager">
+      <section className="propertyManager contentSection">
         <div className="heading">
-          <h2>Property Manager aims to reduce communication friction between landlord and tenant.</h2>
+          <h2 className="header">Property Manager aims to reduce communication friction between landlord and tenant.</h2>
         </div>
       </section>      
-      <section className="ourGoals">
+      <section className="ourGoals contentSection">
         <div className="heading">
-          <ul className='goalUl'><h3>We strive to provide:</h3>
+          <ul className='goalUl'><h3 className="headers">We strive to provide:</h3>
             <li className="goal">Clear visibility and access to important property documents</li>
             <li className="goal">Up-to-date progress on renter requests</li>
             <li className="goal">And be seamless one-stop shop for both renters and owners</li>
