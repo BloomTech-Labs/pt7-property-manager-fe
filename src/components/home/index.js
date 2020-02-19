941cd13b7b3872a171298bc647af39376dc0d735
Carlos Mitchell <kakismitchell@gmail.com>
2020-02-18 21:39:00 -0500

    stuff

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-import './homepage.scss'
+import "./homepage.scss";
 
 export default function Home() {
   return (
     <section className="mainContent">
       <div className="heading w-50">
-        <h1>Find your new home with <br></br>
-        Property Manager</h1>
+        <h1>
+          Find your new home with <br></br>
+          Property Manager
+        </h1>
         <button className="getStarted">Get Started</button>
       </div>
       <div className="icon w-50"></div>
