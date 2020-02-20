28bdc0101743df63b9ed182b06f89a85f56261a5
Carlos Mitchell <kakismitchell@gmail.com>
2020-02-20 01:47:49 -0500

    js complete

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Signin.scss";
 
-export default function Login() {
+function SingIn(props) {
+  const [error, setError] = useState();
+  const [data, setData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleChange = event => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    auth()
+      .post("//PLACE BACK END HERE", data)
+      .then(result => {
+        localStorage.setItem("token", result.data.token);
+        console.log(result.data);
+      })
+      .catch(err => {
+        setError(err.response.data.message);
+      });
+  };
+
   return (
-    <div>
-      <h2>Login Page</h2>
-    </div>
+    <form onSubmit={handleSubmit}>
+      {error && <div className="error">{error}</div>}
+
+      <input
+        type="text"
+        name="email"
+        placeholder="Username"
+        value={data.email}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={data.password}
+        onChange={handleChange}
+      />
+
+      <button type="submit">SingIn</button>
+    </form>
   );
 }
+
+export default SingIn;
