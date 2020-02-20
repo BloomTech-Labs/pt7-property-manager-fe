c567c5fccab068bd0891968b43f5694402f387c9
Carlos Mitchell <kakismitchell@gmail.com>
2020-02-20 02:53:16 -0500

    form completed

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,54 +1,51 @@
-<<<<<<< HEAD
 import React from "react";
 import "./homepage.scss";
 
 export default function Home() {
   return (
-    <section className="mainContent">
-      <div className="heading w-50">
-        <h1>
-          Find your new home with <br></br>
-          Property Manager
-        </h1>
-        <button className="getStarted">Get Started</button>
-      </div>
-      <div className="icon w-50"></div>
-    </section>
-=======
-import React from 'react';
-import './homepage.scss'
-
-export default function Home() {
-  return (
     <div>
-      <section className='mainContent contentSection'>
-        <div className='heading'>
-          <h1 className='header'>Find your new home with <br></br>
-          Property Manager</h1>
-          <button className='getStarted'><a href='#propertyManager'>Get Started</a></button>
+      <section className="mainContent contentSection">
+        <div className="heading">
+          <h1 className="header">
+            Find your new home with <br></br>
+            Property Manager
+          </h1>
+          <button className="getStarted">
+            <a href="#propertyManager">Get Started</a>
+          </button>
         </div>
       </section>
-      <section id='propertyManager' className='propertyManager contentSection'>
-        <div className='heading'>
-          <h2 className='header'>Property Manager aims to reduce communication friction between landlord and tenant.</h2>
+      <section id="propertyManager" className="propertyManager contentSection">
+        <div className="heading">
+          <h2 className="header">
+            Property Manager aims to reduce communication friction between
+            landlord and tenant.
+          </h2>
         </div>
-      </section>      
-      <section className='ourGoals contentSection'>
-        <div className='heading'>
-          <ul className='goalUl header'><h3>We strive to provide:</h3>
-            <li className='goal'>Clear visibility and access to important property documents</li>
-            <li className='goal'>Up-to-date progress on renter requests</li>
-            <li className='goal'>And be seamless one-stop shop for both renters and owners</li>
+      </section>
+      <section className="ourGoals contentSection">
+        <div className="heading">
+          <ul className="goalUl header">
+            <h3>We strive to provide:</h3>
+            <li className="goal">
+              Clear visibility and access to important property documents
+            </li>
+            <li className="goal">Up-to-date progress on renter requests</li>
+            <li className="goal">
+              And be seamless one-stop shop for both renters and owners
+            </li>
           </ul>
         </div>
       </section>
-      <section className='messaging contentSection'>
-        <div className='heading'>
-          <h2 className='header'>Messaging made easier</h2>
-          <h3>With Property Manager, message your landlord, submit work orders, and set alerts without switching between apps.</h3>
+      <section className="messaging contentSection">
+        <div className="heading">
+          <h2 className="header">Messaging made easier</h2>
+          <h3>
+            With Property Manager, message your landlord, submit work orders,
+            and set alerts without switching between apps.
+          </h3>
         </div>
-      </section>    
+      </section>
     </div>
->>>>>>> 7258deac4b3d45c402d92f5cb85c6f0adfc62dd0
   );
 }
--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import "./Signin.scss";
+import "./Sigin.scss";
+import axios from "axios";
 
 function SingIn(props) {
   const [error, setError] = useState();
@@ -8,6 +9,7 @@ function SingIn(props) {
     password: ""
   });
 
+  console.log(data);
   const handleChange = event => {
     setData({
       ...data,
@@ -18,7 +20,9 @@ function SingIn(props) {
   const handleSubmit = event => {
     event.preventDefault();
 
-    auth()
+    //auth() what type of auth we will use
+
+    axios
       .post("//PLACE BACK END HERE", data)
       .then(result => {
         localStorage.setItem("token", result.data.token);
@@ -36,7 +40,9 @@ function SingIn(props) {
       <input
         type="text"
         name="email"
-        placeholder="Username"
+        placeholder="example@domain.com"
+        onFocus={e => (e.target.placeholder = "")}
+        onBlur={e => (e.target.placeholder = "example@domain.com")}
         value={data.email}
         onChange={handleChange}
       />
@@ -44,11 +50,15 @@ function SingIn(props) {
         type="password"
         name="password"
         placeholder="Password"
+        onFocus={e => (e.target.placeholder = "")}
+        onBlur={e => (e.target.placeholder = "Password")}
         value={data.password}
         onChange={handleChange}
       />
 
-      <button type="submit">SingIn</button>
+      <button className="submit" type="submit">
+        SingIn
+      </button>
     </form>
   );
 }
