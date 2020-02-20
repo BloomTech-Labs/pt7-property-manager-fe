35506c49d02e42f523d2c1c608a4de2c652a83f8
Carlos Mitchell <kakismitchell@gmail.com>
2020-02-20 00:51:33 -0500

    styled and replaced dropdown

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
-import './SignUp.scss'
+import "./SignUp.scss";
 //import { axiosAuthenticate } from "../axiosAuthenticate/axiosAuthenticate";
 
 const SignUp = () => {
   const defaul = {
-    first_name:"",
-    last_name:"",
+    first_name: "",
+    last_name: "",
     email: "",
     password: "",
-    phone_number:"",
-    // role:[]  
+    phone_number: "",
+    role: ""
   };
 
   const [register, setRegister] = useState(defaul);
@@ -18,12 +18,12 @@ const SignUp = () => {
   const handleChange = e => {
     setRegister({ ...register, [e.target.name]: e.target.value });
   };
-
+  console.log(register);
   const handleSubmit = e => {
     e.preventDefault();
     // axiosAuthenticate() // ?? How are we gonna handle Auth
     axios
-      .post("Link HERE", register) // end point here!! 
+      .post("Link HERE", register) // end point here!!
       .then(res => {
         console.log(res);
         localStorage.setItem("token", res.data.token);
@@ -41,63 +41,82 @@ const SignUp = () => {
         <h1>Create an Account</h1>
         <form autoComplete="new-password" onSubmit={handleSubmit}>
           <li>
-            <p>Fisrt Name</p>
+            <p className="title">Fisrt Name</p>
             <input
               type="text"
               name="first_name"
               value={register.first_name}
               onChange={handleChange}
-              className="inputField"
+              placeholder={"First Name"}
+              className="First_Name"
             />
           </li>
 
           <li>
-            <p>Last Name</p>
+            <p className="title">Last Name</p>
             <input
               type="text"
               name="last_name"
               value={register.last_name}
               onChange={handleChange}
-              className="inputField"
+              placeholder={"Last Name"}
+              className="Last_Name"
             />
           </li>
 
           <li>
-            <p>Email</p>
+            <p className="title">Email</p>
             <input
               type="email"
               name="email"
               value={register.email}
               onChange={handleChange}
-              className="inputField"
+              placeholder={"email"}
+              className="email"
             />
           </li>
 
           <li>
-            <p>Password</p>
+            <p className="title">Password</p>
             <input
               type="password"
               name="password"
               value={register.password}
               onChange={handleChange}
-              className="inputField"
+              placeholder={"Password"}
+              className="password"
             />
           </li>
 
           <li>
-            <p>Phone Number</p>
+            <p className="title">Phone Number</p>
             <input
               type="tel"
               name="phone_number"
               value={register.phone_number}
               onChange={handleChange}
-              className="inputField"
+              placeholder={"Phone Number"}
+              className="tel"
             />
           </li>
 
+          <li>
+            <p className="title">Are you a ...</p>
+            <select
+              type="checkbox"
+              name="role"
+              value={register.role}
+              onChange={handleChange}
+              className="inputField_checkbox"
+            >
+              <option value="landlord">Landlord</option>
+              <option value="tenant">Tenant</option>
+            </select>
+          </li>
 
-
-          <button type="submit">Join Now</button>
+          <button className="submit" type="submit">
+            Join Now
+          </button>
         </form>
       </div>
     </>
