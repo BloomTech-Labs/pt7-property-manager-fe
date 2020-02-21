4c1f00011c517d330f5e01a3bae549d9604c8a57
Caleb Redd <caleb.redd@yahoo.com>
2020-02-20 22:49:51 -0700

    More backbone structuring in prep for properties and renters. Also for manager's info to be viewable in relation to properties they manage

---
+++ src/components/renter/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Renter(){
+
+  return(
+    <div className="main-content">
+      <h2>Renter's Dashbaord Will Be Here</h2>
+    </div>
+  
+  )}
