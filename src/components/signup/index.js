2c3bf1ceef0c21f9400bdfcb1c03365c58507ccd
Carlos Mitchell <kakismitchell@gmail.com>
2020-03-03 22:03:23 -0500

    manager and renters

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -118,8 +118,8 @@ const SignUp = (props) => {
               className="inputField_checkbox" required
             >
               <option value="">Please choose one option</option>
-              <option value="landlord">Landlord</option>
-              <option value="tenant">Tenant</option>
+              <option value="Renter">Renter</option>
+              <option value="Manager">Manager</option>
             </select>
             </div>
          </div>
