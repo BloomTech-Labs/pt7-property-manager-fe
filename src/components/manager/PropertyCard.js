358b25224dbf9143e80e9bbde7f2225227b72eef
Carlos Sanchez <carlo.s16@hotmail.com>
2020-04-11 23:15:56 -0400

    added styling for property card and responsiveness

--- src/components/manager/PropertyCard.js
+++ src/components/manager/PropertyCard.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import './index.scss'
+
 export default function PropertyCard(props){
   let {property}=props;
   //console.log(property);
@@ -17,9 +19,23 @@ export default function PropertyCard(props){
   }, []);
 
   return(
-    <div style={{minHeight:"30vh",margin:"20px", display:"flex", flexDirection:'column', alignItems:'center', justifyContent:'space-evenly'}}>
+    <div className='propertiesOwned'>
       <h3>{property.address} in {property.city+", "+property.state}</h3>
       <img src={property.img} alt="Insert location into Property Table to display"/>
     </div>  
   )
 }
+
+
+/**
+ style = {
+   {
+     minHeight: "30vh",
+     margin: "20px",
+     display: "flex",
+     flexDirection: 'column',
+     alignItems: 'center',
+     justifyContent: 'space-evenly'
+   }
+ }
+ */
