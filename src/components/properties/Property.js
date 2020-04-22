71a5eeba8435122688570db6f172f9acbf8988bb
Caleb Redd <caleb.redd@yahoo.com>
2020-04-21 21:29:22 -0700

    Button links to add unit and edit unit forms. Cancel buttons on form pages. Links to more info for accessing unit page from manager dashboard. Formatted properties and dashboard pages to accommodate units

--- src/components/properties/Property.js
+++ src/components/properties/Property.js
@@ -42,50 +42,36 @@ export default function Property(props){
       });
   }, [props.match.params.property_id]);
   return (
-    <div className="propertyCard">
-    <div className='propInfo contentSection'>
-      <h2 className='propertyTitle'>{property.address}<br/>{property.city+", "+property.state+" "+property.zip}</h2>
-      <p style={{fontSize:"3rem"}} className='managedBy'>
-        Managed by{" "}
-        <Link to={`/manager/${property.manager_id}`}> {manager.firstName+" "+manager.lastName}</Link>{" "}
-      </p>
-      <p style={{fontSize:"2rem"}}>
-		{property.address}
-      </p>
-      <p style={{fontSize:"2rem"}}>
-		{property.city}, {property.state} {property.zip}
-      </p>
-      <p style={{fontSize:"2rem"}}>
-		{property.country}
-      </p>
-	  <div> 
-		{[...units].map(unit=>{
-			if(unit.property_id == property.id){
-			return(
-		  <div class="my-5  w-100 mx-auto" style={{maxWidth:"1000px", minHeight:"300px", fontSize:"1.5rem"}} > 
-				<hr/>
-			<p key={unit.id}>Unit {unit.number} - Available {Date(unit.date_available)}</p>
-				<div class="">
-					<p key={unit.id+"Description"}>{unit.description}</p>
-					<Link to={`/Properties/${props.match.params.property_id}/Unit/${unit.id}`}><button class="btn btn-primary mx-2 btn-lg">More Info</button></Link>
-					<Link to={`/Properties/${props.match.params.property_id}/Unit/${unit.id}/Apply`}><button class="btn btn-success mx-2 btn-lg">Apply Now</button></Link>
-			  </div> 
-		  </div> 
-		)}})}
-	  </div>
-	  
-    </div>
-    <div className='contentSection'>
-      <img
-        src={property.img}
-        alt="Insert into Property Table to display"
-        className='propertyImg'
-      />      
-    </div>
-    <div className='contentSection units'>
-      units will go here
-    </div>
-
+    <div className="row propertyCard">
+		<div className='col-12 propInfo contentSection'>
+		  <h2 className='propertyTitle'>{property.address}<br/>{property.city+", "+property.state+" "+property.zip}</h2>
+		  <p style={{fontSize:"3rem"}} className='managedBy'>
+			Managed by{" "}
+			<Link to={`/manager/${property.manager_id}`}> {manager.firstName+" "+manager.lastName}</Link>{" "}
+		  </p>
+		</div>
+		<div className='col-6 contentSection'>
+		  <img
+			src={property.img}
+			alt="Insert into Property Table to display"
+			className='propertyImg'
+		  />      
+		</div>
+		<div className='col-6 contentSection units'>
+			{[...units].map(unit=>{
+				if(unit.property_id == property.id){
+					return(
+					  <div class="my-5  w-100 mx-auto" style={{maxWidth:"1000px", minHeight:"100px", fontSize:"1.5rem"}} > 
+						<p key={unit.id}>Unit {unit.number} - Available {Date(unit.date_available)}</p>
+						<p key={unit.id+"Description"}>{unit.description}</p>
+						<Link to={`/Properties/${props.match.params.property_id}/Unit/${unit.id}`}><button class="btn btn-primary mx-2 btn-lg">More Info</button></Link>
+						<Link to={`/Properties/${props.match.params.property_id}/Unit/${unit.id}/Apply`}><button class="btn btn-success mx-2 btn-lg">Apply Now</button></Link>
+						<hr/>
+					  </div> 
+					)
+				}
+			})}
+		</div>
     </div>
   );
 }
